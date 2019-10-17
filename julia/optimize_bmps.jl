0faa87766f3757577214e8bed2662d2dea7b6ac9
fredcallaway <fredc@llaway.com>
2019-10-16 22:17:42 -0400

    remove many old files

--- julia/optimize_bmps.jl
+++ julia/optimize_bmps.jl
@@ -1,8 +1,9 @@
 using Distributed
 using Printf
 using Sobol
-using OnlineStats
 using Statistics
+include("gp_min.jl")
+
 
 function max_cost(m::MetaMDP)
     θ = [1., 0, 0, 1]
@@ -38,10 +39,50 @@ function x2theta(mc, x)
     [x[1] * mc; voi_weights]
 end
 
-include("gp_bmps.jl")
+
+function mean_reward(policy, n_roll, parallel)
+    if parallel
+        rr = @distributed (+) for i in 1:n_roll
+            rollout(policy, max_steps=200).reward
+        end
+        return rr / n_roll
+    else
+        rr = mapreduce(+, 1:n_roll) do i
+            rollout(policy, max_steps=200).reward
+        end
+        return rr / n_roll
+    end
+
+end
 
 
+function optimize_bmps(m::MetaMDP; α=Inf, n_iter=500, seed=nothing, n_roll=10000,
+                  verbose=false, parallel=true, repetitions=1)
+    if seed != nothing
+        Random.seed!(seed)
+    end
+    mc = max_cost(m)
 
+    function loss(x, nr=n_roll)
+        policy = BMPSPolicy(m, x2theta(mc, x), α)
+        reward, secs = @timed mean_reward(policy, n_roll, parallel)
+        if verbose
+            print("θ = ", round.(x2theta(mc, x); digits=2), "   ")
+            @printf "reward = %.3f   seconds = %.3f\n" reward secs
+            flush(stdout)
+        end
+        -reward
+    end
+
+    opt = gp_minimize(loss, 3, noisebounds=[-4, -2],
+                      iterations=n_iter, repetitions=repetitions,
+                      verbose=false)
+
+    f_mod = loss(opt.model_optimizer, 10 * n_roll)
+    f_obs = loss(opt.observed_optimizer, 10 * n_roll)
+    best = f_obs < f_mod ? opt.observed_optimizer : opt.model_optimizer
+    return BMPSPolicy(m, x2theta(mc, best), α)
+end
 
 
 
