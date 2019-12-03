a994ea6cdf7e77feb168ea61e5db9f2707a7ef51
fredcallaway <fredc@llaway.com>
2019-12-02 18:08:20 -0800

    plots checkpoint

--- julia/plots.jl
+++ julia/plots.jl
@@ -5,61 +5,106 @@ include("box.jl")
 using Glob
 using StatsBase
 plot([1,2])
-# %% ====================  ====================
-
-filter(get_results("two_items_fixed")) do res
-    exists(res, :reopt()) || return false
-end
-
-
-# %% ==================== Joint fitting results ====================
-run_name = "joint_fit"
-both_trials = load_dataset.(["two", "three"])
-
-# res = get_result("results/both_items_fixed/2019-10-26T16-32-24-O5k/")
-# res = get_result("results/both_items_fixed_parallel_post/2019-10-28T12-31-17-5Zc/")
-res = get_result("results/test_post/2019-10-28T16-01-38-Hly/")
-prm = load(res, :mle)
-
-res = get_result("results/fit_pseudo/2019-11-06T00-07-11-Gsg")
-load(res, :mle)
-
-res = get_result("results/fit_pseudo/2019-11-06T00-07-11-2Ga")
-load(res, :mle)
-
-function train_test_split(trials, fold)
-    train_idx = Dict(
-        "odd" => 1:2:length(trials),
-        "even" => 2:2:length(trials),
-        "all" => 1:length(trials),
-    )[fold]
-    test_idx = setdiff(eachindex(trials), train_idx)
-    (train=trials[train_idx], test=trials[test_idx])
-end
-
-fold = load(res, :args)["fold"]
-both_trials = map(["two", "three"]) do n
-    trials = load_dataset(n)
-    # train_test_split(trials, fold).test
-end
-
-empirical_prior(trials) = juxt(mean, std)(flatten(trials.value))
-emp_priors = map(both_trials) do trials
-    empirical_prior(trials)
-end
 
 # %% ====================  ====================
-reopt = load(res, :reopt);
+function load_recent_results(dataset)
+    name = (dataset == "both") ? "both_inner" : "test"
+    all_res = filter(get_results(name)) do res
+        exists(res, :reopt) || return false
+        length(load(res, :xy).y) >= 400 || return false
+        args = load(res, :args)
+        args["dataset"] == dataset || return false
+        "n_inner" in keys(args) || return false
+        # length(load(res, :xy).y) == 550 || return false
+    end
+end
+
+function best_result(dataset)
+    ress = load_recent_results(dataset)
+    ress[argmin(results_table(ress).train_loss)]
+end
+
+both_trials = load_dataset.(["two", "three"]);
+
+
+# %% ==================== Summarize fits ====================
+res = load_recent_results("both")[1]
+param_names = [free(load(res, :outer_space)); free(load(res, :inner_space))]
+
+function load_x(res)
+    args = load(res, :args)
+    mle = load(res, :mle)
+    os = load(res, :outer_space)
+    is = load(res, :inner_space)
+    t = type2dict(mle)
+    vcat(os(t), is(t))
+end
+
+mkpath("figs/fits/")
+function plot_fits(results, name; kws...)
+    f = plot(
+        xticks=(1:5, string.(param_names)),
+        ylabel="Normalized parameter value",
+        title=name)
+    for res in results
+        x = load_x(res)
+        # plot!(x.x, color=lab[x.fold], lw=2)
+        plot!(x, lw=2; ylim=(0,1), kws...)
+    end
+    savefig("figs/fits/$name.pdf")
+    display(f)
+end
+
+open("fits.txt", "w") do f
+    for dataset in ["two", "three", "both"]
+        all_res = load_recent_results(dataset)
+        println(f, "\n\n********* $dataset *********",)
+        plot_fits(all_res, dataset)
+        show(f, results_table(all_res), allcols=true, splitcols=false)
+    end
+end
+
+
+# %% ==================== Separate fitting results ====================
+
+ress = best_result.(["two", "three"])
+run_name = "separate_fit_nov25"
+
 both_sims = map(1:2) do i
+    res = ress[i]
     trials = both_trials[i]
-    μ_emp, σ_emp = empirical_prior(trials)
-    policies = reopt[i].policies
-    asyncmap(policies) do pol
-        simulate_experiment(pol, trials; μ=prm.β_μ * μ_emp, σ=prm.β_σ * σ_emp,
+    ds = load(res, :datasets)[1]
+    prm = load(res, :mle)
+    policies = load(res, :reopt)[1].policies
+    asyncmap(policies; ntasks=2) do pol
+        @assert pol.α == prm.α
+        simulate_experiment(pol, trials; μ=prm.β_μ * ds.μ_emp, σ=prm.β_σ * ds.σ_emp,
             sample_time=prm.sample_time, n_repeat=10)
     end
-end;
-length.(both_sims)
+end
+
+# %% ==================== Joint fitting results ====================
+run_name = "joint_fit_dec2"
+function get_sims(res; load_previous=true)
+    load_previous && exists(res, :both_sims) && return load(res, :both_sims)
+    reopt = load(res, :reopt);
+    prm = load(res, :mle)
+    datasets = load(res, :datasets);
+    both_sims = map(1:2) do i
+        trials = both_trials[i]
+        μ_emp, σ_emp = datasets[i].μ_emp, datasets[i].σ_emp
+        policies = reopt[i].policies
+        asyncmap(policies) do pol
+            simulate_experiment(pol, trials; μ=prm.β_μ * μ_emp, σ=prm.β_σ * σ_emp,
+                sample_time=prm.sample_time, n_repeat=10)
+        end
+    end
+    save(res, :both_sims, both_sims)
+    return both_sims
+end
+multi_sims = get_sims.(load_recent_results("both"))
+# res = best_result("both")
+# both_sims = get_sims(res)
 
 # %% ====================  ====================
 function make_lines!(xline, yline, trials)
@@ -75,18 +120,21 @@ function make_lines!(xline, yline, trials)
     end
 end
 
-
 function plot_one(feature, xlab, ylab, trials, sims, plot_kws=();
         binning=nothing, type=:line, xline=nothing, yline=nothing,
         save=false, name=string(feature), kws...)
     hx, hy = feature(trials; kws...)
     bins = make_bins(binning, hx)
     f = plot(xlabel=xlab, ylabel=ylab; plot_kws...)
-
     plot_human!(bins, hx, hy, type)
+
+    if FAST
+        sims = sims[1:2]
+    end
+
     for sim in sims
         mx, my = feature(sim; kws...)
-        plot_model!(bins, mx, my, type, alpha=0.5)
+        plot_model!(bins, mx, my, type, alpha=0.5, color=:blue)
     end
     make_lines!(xline, yline, trials)
     if save
@@ -94,6 +142,8 @@ function plot_one(feature, xlab, ylab, trials, sims, plot_kws=();
     end
     f
 end
+combinedims(multi_sims) |> (x->reshape(x, :, 2)) |> splitdims
+
 
 function plot_one(name::String, xlab, ylab, trials, sims, plot_kws;
         xline=nothing, yline=nothing,
@@ -101,13 +151,18 @@ function plot_one(name::String, xlab, ylab, trials, sims, plot_kws;
 
     f = plot(xlabel=xlab, ylabel=ylab; plot_kws...)
 
+    if FAST
+        sims = sims[1:2]
+    end
+
     plot_human(trials)
     for sim in sims
-        plot_model(sim)
+        plot_model(sim, color=:blue)
     end
     make_lines!(xline, yline, trials)
     f
 end
+
 
 # left_rv = n_item == 2 ? "Left rating - right rating" : "Left rating - mean other rating"
 # best_rv = n_item == 2 ? "Best rating - worst rating" : "Best rating - mean other rating"
@@ -124,8 +179,6 @@ function plot_both(feature, xlab, ylab, plot_kws=(); align=:default, name=string
     # end
     # savefig(ff, "figs/$run_name/$name.pdf")
     # return ff
-
-
 
     f1 = plot_one(feature, xlab1, ylab, both_trials[1], both_sims[1], plot_kws; kws...)
     f2 = plot_one(feature, xlab2, ylab, both_trials[2], both_sims[2], plot_kws; kws...)
@@ -151,20 +204,22 @@ function plot_both(feature, xlab, ylab, plot_kws=(); align=:default, name=string
         name *= "_$(kws[:fix_select])"
     end
     savefig(ff, "figs/$run_name/$name.pdf")
-    nothing
+    return ff
 end
 
 include("features.jl")
+include("plots_base.jl")
+
 
 # %% ==================== Basic psychometrics ====================
-run_name = "indinit"
-# run_name = "nov8_both_400"
+FAST = true
 mkpath("figs/$run_name")
 
 plot_both(value_choice, :left_rv, "P(left chosen)";
     xline=0, yline=:chance, binning=Binning(-4.5:1:4.5))
 
-plot_both(difference_time, :best_rv, "Total fixation time [ms]")
+plot_both(difference_time, :best_rv, "Total fixation time [m]",
+    binning=:integer)
 
 plot_both("rt_kde", "Total fixation time [ms]", "Density", (yticks=[],),
     plot_human=(trials)->kdeplot!(sum.(trials.fix_times), 300., xmin=0, xmax=6000, line=(:black, 2)),
@@ -242,7 +297,10 @@ plot_one(fix3_uncertain,
     both_trials[2], both_sims[2], save=true)
 
 
+
+
 # %% ==================== Choice biases ====================
+
 plot_both(last_fix_bias, "Last fixated item relative rating", "P(last fixated item chosen)",
     binning=:integer; xline=0, yline=:chance)
 
@@ -257,33 +315,3 @@ plot_both(fixation_bias_corrected, "Final time advantage left [ms]", "corrected 
 
 plot_both(first_fixation_duration, "First fixation duration [ms]", "P(first fixated chosen)",
     )
-
-
-# %% ====================  ====================
-
-function fixate_on_(trials, which; sample_time=10, cutoff=2000, n_bin=8, nonfinal=false)
-    n_sample = Int(cutoff / sample_time)
-    spb = Int(n_sample/n_bin)
-    x = Int[]
-    y = Float64[]
-    for t in trials
-        unique_values(t) || continue
-        fix = discretize_fixations(t; sample_time=sample_time)
-        # fix = fix[1:n_sample]
-        ns = min(n_sample, length(fix))
-        fix_best = fix[1:ns] .== which(t.value)
-        props = mean.(Iterators.partition(fix_best, spb))
-        if nonfinal
-            props = props[1:end-1]
-        end
-
-        push!(x, eachindex(props)...)
-        push!(y, props...)
-    end
-    x, y
-end
-
-plot_both(fixate_on_best, "Time since trial onset [ms]", "P(fixate best)",
-    # (xticks=(0.5:2:10.5, string.(0:500:2000)), xlims=(0,8.5)),
-    binning=:integer, yline=:chance, align=:chance,
-    cutoff=3000, n_bin=10)
--- julia/plots_base.jl
+++ julia/plots_base.jl
@@ -4,8 +4,8 @@ nprocs() == 1 && addprocs()
     cd("/usr/people/flc2/juke/choice-eye-tracking/julia/")
     include("model_base.jl")
     include("dc.jl")
+    include("features.jl")
 end
-include("features.jl")
 using Serialization
 using StatsPlots
 plot([1,2])
@@ -14,34 +14,35 @@ plot([1,2])
 pyplot(label="")
 Plots.scalefontsizes()
 Plots.scalefontsizes(1.5)
-const N_BOOT = 1000
-using Bootstrap
 using Printf
-estimator = mean
-ci = 0.95
+
 RED = colorant"#FF6167"
 
-function ci_err(estimator, y)
-    return sem(y) * 2
-    bs = bootstrap(estimator, y, BalancedSampling(N_BOOT))
-    c = confint(bs, BasicConfInt(ci))[1]
+FAST = false
+const CI = 0.95
+const N_BOOT = 1000
+using Bootstrap
+function ci_err(y)
+    FAST && return (sem(y) * 2, sem(y) * 2)
+    isempty(y) && return (NaN, NaN)
+    bs = bootstrap(mean, y, BasicSampling(N_BOOT))
+    c = confint(bs, BCaConfInt(CI))[1]
     abs.(c[2:3] .- c[1])
 end
 
 function plot_human!(bins, x, y, type=:line; kws...)
     vals = bin_by(bins, x, y)
-    @time e = ci_err.(estimator, vals)
-    println(e)
+    err = ci_err.(vals)
     if type == :line
-        plot!(mids(bins), estimator.(vals), yerr=ci_err.(estimator, vals),
+        plot!(mids(bins), mean.(vals), yerr=err,
               grid=:none,
               line=(2,),
               color=:black,
               label="";
               kws...)
     elseif type == :discrete
-        Plots.bar!(mids(bins), estimator.(vals),
-            yerr=ci_err.(estimator, vals),
+        Plots.bar!(mids(bins), mean.(vals),
+            yerr=err,
               grid=:none,
               fill=:white,
               line=(2,),
@@ -62,20 +63,25 @@ end
 
 function plot_model!(bins, x, y, type=:line; kws...)
     vals = bin_by(bins, x, y)
+    err = invert(ci_err.(vals))
+    plot_model!(mids(bins), mean.(vals), err, type; kws...)
+end
+
+function plot_model!(x::Vector{Float64}, y, err, type; color=RED, kws...)
     if type == :line
-        plot!(mids(bins), estimator.(vals),
-              ribbon=ci_err.(estimator, vals),
+        plot!(x, y,
+              ribbon=err,
               fillalpha=0.1,
-              color=RED,
-              line=(RED, 1),
+              # color=color,
+              line=(color, 1),
               label="";
               kws...)
     elseif type == :discrete
-        plot!(mids(bins), estimator.(vals),
-              # yerr=ci_err.(estimator, vals),
+        plot!(x, y,
+              yerr=err,
               grid=:none,
-              line=(RED, 1),
-              marker=(7, :diamond, RED, stroke(0)),
+              line=(color, 1),
+              marker=(7, :diamond, color, stroke(0)),
               label="";
               kws...)
     else
@@ -83,12 +89,6 @@ function plot_model!(bins, x, y, type=:line; kws...)
     end
 end
 
-function plot_model!(sim, feature::Function, bins=nothing, type=:line; kws...)
-    @assert false
-    hx, hy = feature(sim)
-    bins = make_bins(bins, hx)
-    plot_human!(bins, hx, hy, type; kws...)
-end
 
 function cross!(x, y)
     vline!([x], line=(:grey, 0.7), label="")
@@ -127,15 +127,25 @@ function kdeplot!(x, bw::Float64; xmin=quantile(x, 0.05), xmax=quantile(x, 0.95)
 end
 
 using DataFrames
-type2nt(p) = (;(v=>getfield(p, v) for v in fieldnames(typeof(p)))...)
-function results_table(results)
-    map(results) do res
+
+function results_table(results; drop_constant=true)
+    tbl = map(results) do res
         mle = load(res, :mle)
+        mle = (i=0, n_obs=0, mle...)
+        reopt = load(res, :reopt)[1]
+
         # x = @Select(σ_obs, sample_cost, switch_cost, α, μ)(mle)
-        x = type2nt(mle)
-        (x...,
-         train_loss=mean(x[1] / x[3] for x in load(res, :reopt_like)),
-         test_loss=mean(x[1] / x[3] for x in load(res, :test_like))
+        (mle...,
+         train_loss=mean(x[1] / x[3] for x in reopt.train_like),
+         test_loss=mean(x[1] / x[3] for x in reopt.test_like)
          )
     end |> DataFrame
+    delete!(tbl, :i)
+    delete!(tbl, :n_obs)
+    if drop_constant
+        for k in names(tbl)
+            length(unique(tbl[:, k])) == 1 && delete!(tbl, k)
+        end
+    end
+    tbl
 end
